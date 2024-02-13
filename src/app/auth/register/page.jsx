949f4070c35585f59d6d30aaf76c9b243fe5c910
aronresni <aronresni@gmail.com>
2024-02-13 00:14:32 -0300

    upload

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -3,10 +3,30 @@ import React from 'react'
 import { useForm } from "react-hook-form"
 const RegisterPage = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = handleSubmit(async (data) => {
+        if (data.password !== data.confirmPassword) {
+            return alert("Passwords do not match");
+        }
 
-    const onSubmit = handleSubmit(data => {
-        console.log(data);
-    })
+        const res = await fetch("/api/auth/register", {
+            method: "POST",
+            body: JSON.stringify({
+                username: data.username,
+                email: data.email,
+                password: data.password,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (res.ok) {
+            router.push("/auth/login");
+        }
+    });
+
+    console.log(errors);
+
     return (
         <div
             className='h-[calc(100vh-7rem)] flex justify-center items-center'>
@@ -18,6 +38,7 @@ const RegisterPage = () => {
                 <label htmlFor='username' className='text-slate-500 mb-2 block text-sm' >Username</label>
 
                 <input type="text"
+                    placeholder='JhonDoe'
                     {...register("username", {
                         required: {
                             value: true,
@@ -29,32 +50,66 @@ const RegisterPage = () => {
                 {
                     errors.username && (
                         <span className='text-red-500'>
-                            {errors.username}
+                            {errors.username.message}
                         </span>
                     )
                 }
                 <label htmlFor='email' className='text-slate-500 mb-2 block text-sm' >Email</label>
                 <input type="email"
+                    placeholder='JhonDoe@gmail.com'
                     {...register("email", {
-                        required: true,
+                        required: {
+                            value: true,
+                            message: "Email is required"
+                        }
                     })}
                     className="p-3 rounded block mb-2 bg-slate-950 text-slate-300 w-full"
 
                 />
+                {
+                    errors.email && (
+                        <span className='text-red-500'>
+                            {errors.email.message}
+                        </span>
+                    )
+                }
                 <label htmlFor='password' className='text-slate-500 mb-2 block text-sm' >Password</label>
                 <input type="password"
+
+                    placeholder='********'
                     {...register("password", {
-                        required: true,
+                        required: {
+                            value: true,
+                            message: "Password is required"
+                        }
                     })}
                     className="p-3 rounded block mb-2 bg-slate-950 text-slate-300 w-full"
                 />
+                {
+                    errors.password && (
+                        <span className='text-red-500'>
+                            {errors.password.message}
+                        </span>
+                    )
+                }
                 <label htmlFor='password' className='text-slate-500 mb-2 block text-sm' >Confirm Pasword</label>
                 <input type="password"
-                    {...register("password", {
-                        required: true,
+                    placeholder='********'
+                    {...register("confirmPassword", {
+                        required: {
+                            value: true,
+                            message: "Connfirm password please"
+                        }
                     })}
                     className="p-3 rounded block mb-2 bg-slate-950 text-slate-300 w-full"
                 />
+                {
+                    errors.password && (
+                        <span className='text-red-500'>
+                            {errors.password.message}
+                        </span>
+                    )
+                }
                 <button className='w-full bg-blue-500 text-white p-3 rounded-lg font-bold'>Register</button>
 
             </form>
